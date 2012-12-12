17e53d7a23d8d3f94237392b73134febfc59945c
borekk <kostka@tovek.cz>
2012-12-12 15:29:23 +0100

    More efficient  BitSet::nextSetBit

--- src/core/CLucene/util/BitSet.h
+++ src/core/CLucene/util/BitSet.h
@@ -39,6 +39,7 @@ class CLUCENE_EXPORT BitSet:LUCENE_BASE {
   /** Indicates if the bit vector is sparse and should be saved as a d-gaps list, or dense, and should be saved as a bit set. */
   bool isSparse();
   static const uint8_t BYTE_COUNTS[256];
+  static const uint8_t BYTE_OFFSETS[256];
 protected:
 	BitSet( const BitSet& copy );
 
