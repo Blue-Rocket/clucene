74d6b44d0871caca90ce887e85025fd6acca90bb
Matt Magoffin <git+matt@msqr.us>
2014-04-09 11:46:16 +1200

    Fix clang warning "adding int32_t to a string does not append to the string".

--- src/core/CLucene/index/IndexWriter.cpp
+++ src/core/CLucene/index/IndexWriter.cpp
@@ -815,7 +815,7 @@ void IndexWriter::optimize(int32_t maxNumSegments, bool doWait) {
   ensureOpen();
 
   if (maxNumSegments < 1)
-    _CLTHROWA(CL_ERR_IllegalArgument, "maxNumSegments must be >= 1; got " + maxNumSegments);
+	  _CLTHROWA(CL_ERR_IllegalArgument, (string("maxNumSegments must be >= 1; got ") + Misc::toString(maxNumSegments)).c_str());
 
   if (infoStream != NULL)
     message("optimize: index now " + segString());
